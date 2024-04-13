c9cc8a96e55a818a9f5c9faa8131e7b13cf0822f
Shraddha Patel <162610516+ShraddhaPatel888@users.noreply.github.com>
2024-04-13 16:51:08 +0530

    Add files via upload

---
+++ src/Basic_Program/Lab001.java
@@ -0,0 +1,22 @@
+package Basic_Program;
+
+/*        Java Output / Print 
+ * 			1. THE println() method
+ * 			2. The Print() Method
+ */
+
+public class Lab001 {
+
+	public static void main(String[] args) {
+		// 1. THE println() method
+		 System.out.println("Hello World!");
+		    System.out.println("I am learning Java.");
+		    System.out.println("It is awesome!");
+		    
+		    System.out.print("Hello World! ");
+		    System.out.print("I will print on the same line.");
+		//    
+
+	}
+
+}
---
+++ src/Basic_Program/Lab002.java
@@ -0,0 +1,15 @@
+package Basic_Program;
+
+public class Lab002 {
+
+	public static void main(String[] args) {
+		// Java Output Numbers
+		System.out.println(3);
+	    System.out.println(358);
+	    System.out.println(50000);
+	    
+	    System.out.println(3 + 3);
+	    System.out.println(2 * 5);
+	}
+
+}
